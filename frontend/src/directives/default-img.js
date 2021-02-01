619e1c4e3028caff72c8201210e6564eb706142d
somegenki <1159140147@qq.com>
2021-02-01 17:21:06 +0800

    feat 留言完成60%

---
+++ frontend/src/directives/default-img.js
@@ -0,0 +1,39 @@
+import defaultAvatar from '@/assets/images/dio.jpg'
+/**
+ * 检测图片是否存在
+ * @param url 
+ */
+let imageIsExist = function (url) {
+  return new Promise((resolve) => {
+    var img = new Image();
+    img.onload = function () {
+      if (this.complete == true) {
+        resolve(true);
+        img = null;
+      }
+    }
+    img.onerror = function () {
+      resolve(false);
+      img = null;
+    }
+    img.src = url;
+  })
+}
+/**
+ * 当图片加载失败时，显示默认图片
+ * 参数可选：avatar|暂时没了
+ * <img :src="xxxx" v-default-img="'avatar'" />
+ */
+export default async function defaultImg(el, binding) {
+  // 需要显示默认图片(当图片原本的src属性有错时)的类型
+  let params = binding.value;
+  // 图片原本的src
+  let realURL = el.src;
+  // 当原本图片不存在时，根据参数返回不同的图片url
+  let exist = await imageIsExist(realURL);
+  if (!exist) {
+    if (params === 'avatar') {
+      el.setAttribute('src', defaultAvatar);
+    }
+  }
+}
---
+++ frontend/src/views/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="index">11</div>
+</template>
+
+<script>
+export default {
+name:'index',
+}
+</script>
+
+<style>
+
+</style>
